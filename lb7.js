6a1a4607cabca3a004986a996dbda38d3e273171
Feifei Chen <deardogzxx@gmail.com>
2021-03-04 22:20:34 +0000

    Initial commit

---
+++ lb7.js
@@ -0,0 +1,9 @@
+function futr(){
+var in1=document.futureInput.in1.value;
+var in2=document.futureInput.in2.value;
+var in3=document.futureInput.in3.value;
+
+console.log(in1,in2,in3);
+}
+ </body>
+</html>

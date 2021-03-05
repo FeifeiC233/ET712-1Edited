22f602238cf76ab1968672fdfbd05942ec341138
Feifei Chen <deardogzxx@gmail.com>
2021-03-05 04:16:09 +0000

    Deleted everything to start from scratch

--- lb7.js
+++
@@ -1,9 +0,0 @@
-function futr(){
-var in1=document.futureInput.in1.value;
-var in2=document.futureInput.in2.value;
-var in3=document.futureInput.in3.value;
-
-console.log(in1,in2,in3);
-}
- </body>
-</html>
